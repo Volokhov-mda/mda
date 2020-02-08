43a862655239c58a5ec414aa3e1df6201b962c69
Volokhov-mda <60272801+Volokhov-mda@users.noreply.github.com>
2020-02-08 03:04:28 +0300

    The second version of Homework

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -4,117 +4,73 @@ using System.Collections.Generic;
 
 namespace calculator
 {
-    delegate double MathOperation(double a, double b);
+    public delegate double MathOperation(double a, double b);
 
-    class Program
+    public delegate void ErrorNotificationType(string message);
+
+    public class Calculator
     {
-        // Файл, в котором записаны все выражения.
-        const string expressionsFile = "../../../expressions.txt";
-
-        // Файл, в котором записаны заданные заранее результаты выражений.
-        const string checkerFile = "../../../expressions_checker.txt";
-
-        // Файл, в котором записаны результаты выражений, вычесленные программой.
-        const string answersFile = "../../../answers.txt";
-
-        // Файл, в котором записаны результаты сравнения каждого результата выражений из файла checkerFile и answersFile.
-        const string resultsFile = "../../../results.txt";
-
         // Словарь, в котором хранятся арифметические операции.
         static Dictionary<String, MathOperation> operations = new Dictionary<String, MathOperation>();
 
-        static Program()
-        {
-            // Заполнение словаря арифметическими операциями.
-            operations = new Dictionary<string, MathOperation>();
-            operations.Add("+", (x, y) => { return x + y; });
-            operations.Add("-", (x, y) => { return x - y; });
-            operations.Add("*", (x, y) => { return x * y; });
-            operations.Add("/", (x, y) => { return x / y; });
-            operations.Add("^", (x, y) => { return Math.Pow(x, y); });
-        }
-
-        /// <summary>
-        /// Метод, возвращающий результат данного выражения.
-        /// </summary>
-        /// <param name="expr">Выражение</param>
-        /// <returns></returns>
-        static double Calculate(string expr)
-        {
-            // Разбиение выражения на операнды и операцию.
-            string[] arguments = expr.Split();
-
-            double operandA = double.Parse(arguments[0]);
-            string operatorExpr = arguments[1];
-            double operandB = double.Parse(arguments[2]);
-
-            // Результат выражения, вычесленный при помощи словаря.
-            return operations[operatorExpr](operandA, operandB);
-        }
-
-        /// <summary>
-        /// Метод, очищающий или создающий файлы по переданным директориям.
-        /// </summary>
-        /// <param name="paths">Директории файлов, которые требуется очистить/создать</param>
-        static void ClearFiles(params string[] paths)
-        {
-            foreach (var path in paths)
-            {
-                try
-                {
-                    File.WriteAllText(path, "");
-                }
-                catch (FileNotFoundException e)
-                {
-                    Console.WriteLine("File Not Found Exception: ", e.Message);
-                }
-                catch (IOException e)
-                {
-                    Console.WriteLine("Input/Output Exception: ", e.Message);
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    Console.WriteLine("Unauthorized Access Exception: ", e.Message);
-                }
-                catch (System.Security.SecurityException e)
-                {
-                    Console.WriteLine("Security Exception: ", e.Message);
-                }
-            }
-        }
+        // Событие, сообщяющее о возникших ошибках при записи файла.
+        public event ErrorNotificationType ErrorNotification;
+        // Событие, сообщяющее о возникших ошибках в вычислениях.
+        public event ErrorNotificationType CalculatingError;
 
         /// <summary>
         /// Метод, вычисляющий значения каждого выражения, записанного в файл, и записывающий их в новый файл.
         /// </summary>
         /// <param name="expressionsFile">Директория файла, в котором хранятся выражения</param>
         /// <param name="answersFile">Директория файла, в который записываются значения выражений</param>
-        static void CalculateFileExpressions(string expressionsFile, string answersFile)
+        public void CalculateFileExpressions(string expressionsFile, string answersFile)
         {
             // Массив выражений.
             string[] arrOfExprs = File.ReadAllLines(expressionsFile);
 
+            Calculator calculator = new Calculator(operations);
+
             foreach (var expr in arrOfExprs)
             {
+                // Разбиение выражения на операнды и операцию.
+                string[] arguments = expr.Split();
+
+                double operandA = double.Parse(arguments[0]);
+                string operatorExpr = arguments[1];
+                double operandB = double.Parse(arguments[2]);
+
                 try
                 {
                     // Запись в файл answersFile результатов выражений (каждый результат записывается с новой строчки).
-                    File.AppendAllText(answersFile, $"{Calculate(expr):f3}" + Environment.NewLine);
+                    File.AppendAllText(answersFile, $"{operations[operatorExpr](operandA, operandB):f3}" + Environment.NewLine);
+                }
+                catch (KeyNotFoundException)
+                {
+                    CalculatingError("неверный оператор" + Environment.NewLine);
+                }
+                catch (DivideByZeroException)
+                {
+                    CalculatingError("bruh" + Environment.NewLine);
+                }
+                catch (NotFiniteNumberException)
+                {
+                    CalculatingError("не число" + Environment.NewLine);
                 }
                 catch (FileNotFoundException e)
                 {
-                    Console.WriteLine("File Not Found Exception: ", e.Message);
+                    ErrorNotification(e.Message);
                 }
                 catch (IOException e)
                 {
-                    Console.WriteLine("Input/Output Exception: ", e.Message);
+                    ErrorNotification(e.Message);
                 }
                 catch (UnauthorizedAccessException e)
                 {
-                    Console.WriteLine("Unauthorized Access Exception: ", e.Message);
+                    ErrorNotification(e.Message);
                 }
                 catch (System.Security.SecurityException e)
                 {
-                    Console.WriteLine("Security Exception: ", e.Message);
+                    ErrorNotification(e.Message);
                 }
             }
         }
@@ -125,7 +81,7 @@ namespace calculator
         /// <param name="answersFile">Первый файл со значениями выражений</param>
         /// <param name="checkerFile">Второй файл со значениями выражений</param>
         /// <param name="resultsFile">Файл, в который записываются результаты сравнений</param>
-        static void CheckResults(string answersFile, string checkerFile, string resultsFile)
+        public void CheckResults(string answersFile, string checkerFile, string resultsFile)
         {
             try
             {
@@ -140,9 +96,9 @@ namespace calculator
                 // Проверка, одинаковое ли количество результатов веражений в двух файлах.
                 if (answers.Length != checks.Length)
                 {
-                    Console.WriteLine(
+                    Console.WriteLine(Environment.NewLine +
                         "Impossible to check the answers: different number of elements in answers and checker files." + Environment.NewLine +
-                        $"There are {answers.Length} elements in first file and {checks.Length} int the second file.");
+                        $"There are {answers.Length} elements in first file and {checks.Length} in the second file.");
                 }
                 else
                 {
@@ -158,33 +114,124 @@ namespace calculator
             }
             catch (FileNotFoundException e)
             {
-                Console.WriteLine("File Not Found Exception: ", e.Message);
+                ErrorNotification(e.Message);
             }
             catch (IOException e)
             {
-                Console.WriteLine("Input/Output Exception: ", e.Message);
+                ErrorNotification(e.Message);
             }
             catch (UnauthorizedAccessException e)
             {
-                Console.WriteLine("Unauthorized Access Exception: ", e.Message);
+                ErrorNotification(e.Message);
             }
             catch (System.Security.SecurityException e)
             {
-                Console.WriteLine("Security Exception: ", e.Message);
+                ErrorNotification(e.Message);
             }
+        }
+
+        /// <summary>
+        /// Метод, очищающий или создающий файлы по переданным директориям.
+        /// </summary>
+        /// <param name="paths">Директории файлов, которые требуется очистить/создать</param>
+        public void ClearFiles(params string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                try
+                {
+                    File.WriteAllText(path, "");
+                }
+                catch (FileNotFoundException e)
+                {
+                    ErrorNotification(e.Message);
+                }
+                catch (IOException e)
+                {
+                    ErrorNotification(e.Message);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    ErrorNotification(e.Message);
+                }
+                catch (System.Security.SecurityException e)
+                {
+                    ErrorNotification(e.Message);
+                }
+            }
+        }
+
+
+        // Конструктор.
+        public Calculator(Dictionary<string, MathOperation> mathOperations)
+        {
+            operations = mathOperations;
+        }
+    }
+
+    class Program
+    {
+        // Файл, в котором записаны все выражения.
+        const string expressionsFile = "../../../expressions.txt";
+
+        // Файл, в котором записаны заданные заранее результаты выражений.
+        const string checkerFile = "../../../expressions_checker.txt";
+
+        // Файл, в котором записаны результаты выражений, вычесленные программой.
+        const string answersFile = "../../../answers.txt";
+
+        // Файл, в котором записаны результаты сравнения каждого результата выражений из файла checkerFile и answersFile.
+        const string resultsFile = "../../../results.txt";
+
+        /// <summary>
+        /// Метод, выводящий в консоль сообщение об ошибке.
+        /// </summary>
+        /// <param name="message">Сообщение, выводящееся в консоль</param>
+        public static void ConsoleErrorHandler(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.TimeOfDay}: {message}");
+        }
+
+        /// <summary>
+        /// Записывает в файл answersFile сообщение message.
+        /// </summary>
+        /// <param name="message">Сообщение, записываемое в файл</param
+        public static void ResultErrorHandler(string message)
+        {
+            File.AppendAllText(answersFile, "неверный оператор");
+        }
+
+        // Словарь, в котором хранятся арифметические операции.
+        static Dictionary<String, MathOperation> operations = new Dictionary<String, MathOperation>();
+
+        static Program()
+        {
+            // Заполнение словаря арифметическими операциями.
+            operations = new Dictionary<string, MathOperation>();
+            operations.Add("+", (x, y) => { return x + y; });
+            operations.Add("-", (x, y) => { return x - y; });
+            operations.Add("*", (x, y) => { return x * y; });
+            operations.Add("/", (x, y) => { return x / y; });
+            operations.Add("^", (x, y) => { return Math.Pow(x, y); });
         }
 
         static void Main(string[] args)
         {
+            Calculator calculator = new Calculator(operations);
+
+            // Определение событий.
+            calculator.ErrorNotification += ConsoleErrorHandler;
+            calculator.CalculatingError += ResultErrorHandler;
+
             // Очистка/создание файлов.
-            ClearFiles(answersFile, resultsFile);
+            calculator.ClearFiles(answersFile, resultsFile);
 
             // Вычисление значений выражений и их запись в новый файл.
-            CalculateFileExpressions(expressionsFile, answersFile);
+            calculator.CalculateFileExpressions(expressionsFile, answersFile);
 
             // Сравнение посчитанных результатов с результатами, предложенными в файле checkerFile.
             // Запись результатов сравнения в новый файл.
-            CheckResults(answersFile, checkerFile, resultsFile);
+            calculator.CheckResults(answersFile, checkerFile, resultsFile);
         }
     }
 }
